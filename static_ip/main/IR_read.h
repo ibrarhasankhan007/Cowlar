60c1813c384769126daaee87898bbc283cf3903b
Ibrar Hasan <ibrarhasankhan007@gmail.com>
2023-03-08 10:34:54 +0500

    read the IR data through adc take the static ip and send the data through UDP

---
+++ static_ip/main/IR_read.h
@@ -0,0 +1,15 @@
+/*
+ * IR_read.h
+ *
+ *  Created on: Mar 8, 2023
+ *      Author: ibrar_hasan
+ */
+
+#ifndef MAIN_IR_READ_H_
+#define MAIN_IR_READ_H_
+
+int adc_reading;
+void IR_read_init();
+
+
+#endif /* MAIN_IR_READ_H_ */
